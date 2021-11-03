ef4e33b7e76c0a246726862626a175048a2fb65b
Kuroha <KurohaKirito@gmail.com>
2021-11-03 17:13:15 +0800

    添加框架及编辑器工具

---
+++ Assets/Kuroha/Util/Editor/AssetUtil.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Kuroha.Util.RunTime;
+using UnityEditor;
+using UnityEngine;
+
+namespace Kuroha.Util.Editor
+{
+    public static class AssetUtil
+    {
+        public enum FindType
+        {
+            All,
+            EnableOnly,
+            DisableOnly
+        }
+
+        /// <summary>
+        /// 批量删除资源
+        /// 需要一个整合了待删除资源所在路径的文件
+        /// 注意路径必须是 Assets 相对路径
+        /// </summary>
+        /// <param name="assetFilePath">包含待删除资源路径信息的文件</param>
+        public static void DeleteAsset(ref string assetFilePath)
+        {
+            var path = assetFilePath;
+            var counter = 0;
+
+            using (var file = new StreamReader(path))
+            {
+                var line = file.ReadLine();
+
+                while (line.IsNotNullAndEmpty())
+                {
+                    if (UnityEditor.FileUtil.DeleteFileOrDirectory(line))
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        DebugUtil.LogError($"删除失败: {line}");
+                    }
+
+                    line = file.ReadLine();
+                }
+            }
+
+            DebugUtil.Log($"共成功删除了 {counter} 项资源!");
+
+            if (counter > 0)
+            {
+                DebugUtil.Log("请保存并刷新, 让 Unity 自动删除资源对应的 meta 文件!");
+            }
+            
+            assetFilePath = "已执行删除!";
+        }
+
+        /// <summary>
+        /// 获取场景中全部的 Transform
+        /// </summary>
+        /// <param name="type">筛选规则</param>
+        /// <returns></returns>
+        public static List<Transform> GetAllTransformInScene(FindType type)
+        {
+            var result = new List<Transform>();
+            var allTransforms = UnityEngine.Resources.FindObjectsOfTypeAll<UnityEngine.Transform>();
+            
+            foreach (var transform in allTransforms)
+            {
+                switch (type)
+                {
+                    case FindType.All:
+                        if (transform != null)
+                        {
+                            result.Add(transform);
+                        }
+                        break;
+                    
+                    case FindType.EnableOnly:
+                        if (transform != null && transform.gameObject.activeSelf)
+                        {
+                            result.Add(transform);
+                        }
+                        break;
+                    
+                    case FindType.DisableOnly:
+                        if (transform != null && transform.gameObject.activeSelf == false)
+                        {
+                            result.Add(transform);
+                        }
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(type), type, null);
+                }
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// 获取场景中全部的游戏物体
+        /// </summary>
+        /// <param name="type">筛选规则</param>
+        /// <returns></returns>
+        public static List<T> GetAllComponentsInScene<T>(FindType type) where T : UnityEngine.Component
+        {
+            var result = new List<T>();
+            var allTransforms = UnityEngine.Resources.FindObjectsOfTypeAll<UnityEngine.Transform>();
+            
+            foreach (var transform in allTransforms)
+            {
+                if (transform.TryGetComponent<T>(out var component))
+                {
+                    switch (type)
+                    {
+                        case FindType.All:
+                            result.Add(component);
+                            break;
+                    
+                        case FindType.EnableOnly:
+                            if (component.gameObject.activeSelf)
+                            {
+                                result.Add(component);
+                            }
+                            break;
+                    
+                        case FindType.DisableOnly:
+                            if (component.gameObject.activeSelf == false)
+                            {
+                                result.Add(component);
+                            }
+                            break;
+                    
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(type), type, null);
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+        /// <summary>
+        /// 拷贝源文件夹到新的文件夹
+        /// </summary>
+        /// <param name="sourcePath">源文件所在目录</param>
+        /// <param name="savePath">保存的目标目录</param>
+        /// <returns>true:拷贝成功; false:拷贝失败</returns>
+        public static void CopyFolderToAssetsFolder(string sourcePath, string savePath)
+        {
+            var sourceDirs = new List<string>();
+            var sourceFiles = new List<string>();
+
+            sourceDirs.Add(sourcePath);
+            for (var index = 0; index < sourceDirs.Count; index++)
+            {
+                sourceDirs.AddRange(Directory.GetDirectories(sourceDirs[index]));
+                sourceFiles.AddRange(Directory.GetFiles(sourceDirs[index]));
+            }
+
+            DebugUtil.Log($"共需要 {sourceDirs.Count} 个目录, {sourceFiles.Count} 个文件");
+
+            foreach (var sourceDir in sourceDirs)
+            {
+                var targetFolder = sourceDir.Replace(sourcePath, savePath);
+                var newFolderName = targetFolder.Replace('\\', '/').Split('/').Last();
+                var parentFolder = targetFolder.Substring(targetFolder.IndexOf("Assets", StringComparison.OrdinalIgnoreCase));
+                parentFolder = parentFolder.Replace('\\', '/').Replace($"/{newFolderName}", "");
+
+                DebugUtil.Log($"创建目录: {parentFolder}, {newFolderName}");
+                AssetDatabase.CreateFolder(parentFolder, newFolderName);
+            }
+
+            foreach (var sourceFile in sourceFiles)
+            {
+                var targetFile = sourceFile.Replace(sourcePath, savePath);
+                DebugUtil.Log($"创建文件: {targetFile}");
+
+                File.Copy(sourceFile, targetFile, true);
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        /// <summary>
+        /// 将一个资源移动到新的目录下
+        /// </summary>
+        /// <param name="assetFilePath">包含待移动资源路径信息的文件</param>
+        /// <param name="newFolder">目标目录</param>
+        public static void MoveFileToNewFolder(ref string assetFilePath, string newFolder)
+        {
+            var configPath = assetFilePath;
+            var counter = 0;
+
+            using (var file = new StreamReader(configPath))
+            {
+                var line = file.ReadLine();
+
+                while (line.IsNotNullAndEmpty())
+                {
+                    if (MoveAsset(line, newFolder))
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        DebugUtil.LogError($"移动失败: {line}", null, "red");
+                    }
+
+                    line = file.ReadLine();
+                }
+            }
+
+            DebugUtil.Log($"共成功移动了 {counter} 项资源!", null, "green");
+            
+            assetFilePath = "已执行移动!";
+        }
+        
+        /// <summary>
+        /// 将一个资源移动到新的目录下
+        /// </summary>
+        /// <param name="assetPath">资源路径</param>
+        /// <param name="newFolder">目标目录</param>
+        private static bool MoveAsset(string assetPath, string newFolder)
+        {
+            var success = false;
+            var oldPath = assetPath.Replace('\\', '/');
+            newFolder = newFolder.Replace('\\', '/');
+            
+            // 待移动资源
+            var asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(oldPath);
+            if (ReferenceEquals(asset, null) == false)
+            {
+                // 目标路径
+                var path = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(newFolder);
+                if (ReferenceEquals(path, null) == false)
+                {
+                    // 待移动资源完整名称
+                    var assetName = assetPath.Substring(assetPath.LastIndexOf("/", StringComparison.Ordinal));
+                    var newPath = newFolder + assetName;
+
+                    // 移动资源
+                    AssetDatabase.MoveAsset(oldPath, newPath);
+                    
+                    success = true;
+                }
+                else
+                {
+                    DebugUtil.Log("目标路径为空", null, "red");
+                }
+            }
+            else
+            {
+                DebugUtil.Log("资源为空", null, "red");
+            }
+
+            return success;
+        }
+    }
+}
---
+++ Assets/Kuroha/Util/RunTime/DynamicClass.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Reflection;
+
+namespace Kuroha.Util.RunTime
+{
+    /// <summary>
+    /// 动态类
+    /// </summary>
+    public class DynamicClass
+    {
+        #region 反射筛选标志
+
+        /// <summary>
+        /// Public 字段
+        /// </summary>
+        private const BindingFlags PUBLIC_INSTANCE_FIELD =
+            BindingFlags.Public |
+            BindingFlags.Instance |
+            BindingFlags.GetField;
+        
+        /// <summary>
+        /// Private 字段
+        /// </summary>
+        private const BindingFlags PRIVATE_INSTANCE_FIELD =
+            BindingFlags.NonPublic |
+            BindingFlags.Instance |
+            BindingFlags.GetField;
+        
+        /// <summary>
+        /// Private Static 字段
+        /// </summary>
+        private const BindingFlags PRIVATE_STATIC_FIELD =
+            BindingFlags.NonPublic |
+            BindingFlags.Static |
+            BindingFlags.GetField;
+        
+        /// <summary>
+        /// Public 方法
+        /// </summary>
+        private const BindingFlags PUBLIC_INSTANCE_METHOD =
+            BindingFlags.Public |
+            BindingFlags.Instance;
+        
+        /// <summary>
+        /// Public Static 方法
+        /// </summary>
+        private const BindingFlags PUBLIC_STATIC_INSTANCE_METHOD =
+            BindingFlags.Public |
+            BindingFlags.Static |
+            BindingFlags.Instance;
+        
+        /// <summary>
+        /// Private 方法
+        /// </summary>
+        private const BindingFlags PRIVATE_INSTANCE_METHOD =
+            BindingFlags.NonPublic |
+            BindingFlags.Instance;
+
+        #endregion
+        
+        /// <summary>
+        /// 动态类当前所指代的类
+        /// </summary>
+        private readonly Type currentClass;
+
+        /// <summary>
+        /// 动态类当前所指代的类的实例
+        /// </summary>
+        private object currentInstance;
+        
+        /// <summary>
+        /// 设置实例
+        /// </summary>
+        /// <param name="obj"></param>
+        public void SetInstance(object obj)
+        {
+            if (obj.GetType() == currentClass)
+            {
+                currentInstance = obj;
+            }
+        }
+        
+        /// <summary>
+        /// 获取实例
+        /// </summary>
+        /// <returns></returns>
+        public object GetInstance()
+        {
+            return currentInstance;
+        }
+
+        /// <summary>
+        /// 构造函数 (参数: 类型)
+        /// </summary>
+        /// <param name="currentClass"></param>
+        public DynamicClass(Type currentClass)
+        {
+            this.currentClass = currentClass;
+        }
+
+        /// <summary>
+        /// 构造函数 (参数: 类型的实例)
+        /// </summary>
+        /// <param name="obj"></param>
+        public DynamicClass(object obj)
+        {
+            if (obj == null)
+            {
+                return;
+            }
+            
+            currentClass = obj.GetType();
+            currentInstance = obj;
+        }
+
+        #region 获取字段接口
+
+        #region private static
+
+        /// <summary>
+        /// 获取类中的 private static 字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public T GetFieldValue_PrivateStatic<T>(string fieldName) where T : class
+        {
+            return GetFieldValue_PrivateStatic(fieldName) as T;
+        }
+
+        /// <summary>
+        /// 获取类中的 private static 字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public object GetFieldValue_PrivateStatic(string fieldName)
+        {
+            return GetFiledValue(fieldName, PRIVATE_STATIC_FIELD);
+        }
+
+        #endregion
+
+        #region Private
+        
+        /// <summary>
+        /// 获取类中的 private 字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public T GetFieldValue_Private<T>(string fieldName) where T : class
+        {
+            return GetFieldValue_Private(fieldName) as T;
+        }
+
+        /// <summary>
+        /// 获取类中的 private 字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public object GetFieldValue_Private(string fieldName)
+        {
+            return GetFiledValue(fieldName, PRIVATE_INSTANCE_FIELD);
+        }
+
+        #endregion
+
+        #region Public
+
+        /// <summary>
+        /// 获取类中的 public 字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public T GetFieldValue_Public<T>(string fieldName) where T : class
+        {
+            return GetFieldValue_Public(fieldName) as T;
+        }
+        
+        /// <summary>
+        /// 获取类中的 public 字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <returns></returns>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public object GetFieldValue_Public(string fieldName)
+        {
+            return GetFiledValue(fieldName, PUBLIC_INSTANCE_FIELD);
+        }
+        
+        #endregion
+        
+        /// <summary>
+        /// 获取类中的字段的值
+        /// </summary>
+        /// <param name="fieldName"></param>
+        /// <param name="flags"></param>
+        /// <returns></returns>
+        private object GetFiledValue(string fieldName, BindingFlags flags)
+        {
+            if (currentClass == null)
+            {
+                return null;
+            }
+            
+            var dynamicField = currentClass.GetField(fieldName, flags);
+            return dynamicField == null ? null : dynamicField.GetValue(currentInstance);
+        }
+
+        #endregion
+
+        #region 调用函数接口
+
+        /// <summary>
+        /// 调用 public 函数
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="args"></param>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public void CallMethod_Public(string methodName, params object[] args)
+        {
+            InvokeMethod(methodName, PUBLIC_INSTANCE_METHOD, args);
+        }
+        
+        /// <summary>
+        /// 调用 public static 函数
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="args"></param>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public void CallMethod_PublicStatic(string methodName, params object[] args)
+        {
+            InvokeMethod(methodName, PUBLIC_STATIC_INSTANCE_METHOD, args);
+        }
+
+        /// <summary>
+        /// 调用 private 函数
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="args"></param>
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once MemberCanBePrivate.Global
+        public void CallMethod_Private(string methodName, params object[] args)
+        {
+            InvokeMethod(methodName, PRIVATE_INSTANCE_METHOD, args);
+        }
+        
+        /// <summary>
+        /// 调用类中的函数
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="flags"></param>
+        /// <param name="args"></param>
+        private void InvokeMethod(string methodName, BindingFlags flags, params object[] args)
+        {
+            if (currentClass == null) return;
+            var method = currentClass.GetMethod(methodName, flags);
+            if (method == null) return;
+            method.Invoke(currentInstance, args);
+        }
+        
+        #endregion
+        
+        /// <summary>
+        /// 从源实例中取出与目标实例 "同名字段" 的值
+        /// </summary>
+        /// <param name="dstInstance">目标实例</param>
+        /// <param name="dstFlags">目标实例中字段值的类型</param>
+        /// <param name="srcInstance">源实例</param>
+        /// <param name="srcFlags">源实例中字段值的类型</param>
+        public static void Copy(object dstInstance, BindingFlags dstFlags, object srcInstance, BindingFlags srcFlags)
+        {
+            if (dstInstance == null || srcInstance == null)
+            {
+                return;
+            }
+            
+            // 取出两个实例的类型
+            var srcType = srcInstance.GetType();
+            var dstType = dstInstance.GetType();
+            
+            // 取出目标实例中的字段
+            var dstFields = dstType.GetFields(dstFlags);
+            foreach (var dstFieldInfo in dstFields)
+            {
+                // 得到源实例中的 "相同字段"
+                var srcFieldInfo = srcType.GetField(dstFieldInfo.Name, srcFlags);
+                if (srcFieldInfo != null)
+                {
+                    if (dstFieldInfo.FieldType == srcFieldInfo.FieldType)
+                    {
+                        // 取出源字段的值
+                        var value = srcFieldInfo.GetValue(srcInstance);
+                        
+                        // 赋值给目标字段
+                        dstFieldInfo.SetValue(dstInstance, value);
+                    }
+                }
+            }
+        }
+    }
+}
